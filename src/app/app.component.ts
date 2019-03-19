2da4ac00d02cba8202590d738faab111d33016f6
Carlos Ramos <carlos.ramos@decoracontent.com>
2019-03-19 15:08:59 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SocketService } from './services/socket/socket.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'socketio-client';
+
+  messages = [];
+
+  ioConnection: any;
+  
+  constructor(private _service: SocketService) {
+
+  }
+
+  private initIoConnection(): void {
+    this._service.initSocket();
+
+    // this.ioConnection = this._service.onMessage()
+    //   .subscribe((message) => {
+    //     this.messages.push(message);
+    //   });
+
+      this.ioConnection = this._service.onWs()
+      .subscribe((message) => {
+        this.messages.push('ws', message);
+      });      
+
+    this._service.onEvent('CONNECT')
+      .subscribe(() => {
+        console.log('connected');
+      });
+      
+    this._service.onEvent('DISCONNECT')
+      .subscribe(() => {
+        console.log('disconnected');
+      });
+  }
+
+  onInitConnection() {
+    if (this.ioConnection) {
+      return;
+    }
+    this.initIoConnection();
+  }
+
+  onMessage(message: string) {
+    // this._service.send(message);
+    this._service.sendWs(message);
+  }
 }
---
+++ src/app/services/socket/socket.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import * as socketIo from 'socket.io-client';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketService {
+
+  private _socket: any;
+
+  constructor() { }
+
+  public initSocket(): void {
+    this._socket = socketIo(environment.webSocketApi);
+  }
+
+  public send(message: string): void {
+    this._socket.emit('message', message);
+  }
+
+  public sendWs(ws: string): void {
+    this._socket.emit('ws', ws);
+  }
+
+  public onMessage(): Observable<string> {
+    return new Observable( observer => this._socket.on('message', data => observer.next(data)));
+  }
+
+  public onWs(): Observable<string> {
+    return new Observable( observer => this._socket.on('ws', data => observer.next(data)));
+  }
+
+  public onEvent(event): Observable<string> {
+    return new Observable( observer => this._socket.on(event, () => observer.next()));
+  }
+}
